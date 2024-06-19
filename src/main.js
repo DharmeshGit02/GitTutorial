2986d7494af75b81d7b9f06c225572ec620fff0b
Dhamu <dmarathe2002@gmail.com>
2024-06-19 19:23:27 +0530

    Added binary search to script

--- src/main.js
+++ src/main.js
@@ -2,4 +2,19 @@ function sum(arr) {
     return arr.reduce((accum, currVal) => currVal + accum, 0)
 }
 
-console.log(sum([10, 20, 30]))
+function binSearch(arr, key) {
+    const sortedArr = arr.sort((a, b) => a - b)
+    let low = 0, high = sortedArr.length - 1
+    while(low <= high) {
+        let mid = Math.trunc(low + (high - low) / 2)
+        if(sortedArr[mid] == key) return `${key} found at index ${mid}`
+        else if(sortedArr[mid] < key) high = mid - 1
+        else low = mid + 1
+    }
+    return `${key} doesn't exists!`
+}
+
+const arr = [2, 3, 1, 7, 8, 9, 10, 5, 6, 4]
+
+console.log(sum(arr))
+console.log(binSearch(arr, 5))
